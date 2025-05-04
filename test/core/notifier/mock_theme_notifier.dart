549ebf8eaa590d21ddc543cf99c2d0374504099e
Hovsep Hayrapetyan <hoso.hayrapetyan@gmail.com>
2025-05-04 21:17:24 +0400

    [development/test]: add some tests

---
+++ test/core/notifier/mock_theme_notifier.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:ricky_morty/core/notifiers/theme_notifier.dart';
+
+@GenerateMocks([ThemeNotifier])
+void main() {}
---
+++ test/data/service/mock_preference_service.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:ricky_morty/data/service/preference_service_impl.dart';
+
+@GenerateMocks([PreferencesServiceImp])
+void main() {}
